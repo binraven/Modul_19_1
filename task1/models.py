2f2c6ae43682dfa54a0e69f008df9cc31d4c846d
binraven <tihonov-ks@ya.ru>
2025-01-17 16:16:16 +0500

    Add files via upload

--- task1/models.py
+++ task1/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=1000, decimal_places= 3)
-    age = models.IntegerField
+    age = models.IntegerField()
 
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=1000, decimal_places= 3)
     size = models.DecimalField(max_digits=1000, decimal_places= 3)
-    description = models.TextField
-    age_limited = models.BooleanField
+    description = models.TextField()
+    age_limited = models.BooleanField()
     buyer = models.ManyToManyField(Buyer, related_name="games")
